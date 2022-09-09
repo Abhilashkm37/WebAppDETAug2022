91d22344b547c2ddfd4617aaa8ec947cd4e9529c
Abhilash <abhilashkm37@gmail.com>
2022-09-09 09:06:02 +0530

    .NetCore

---
+++ Pages/HelloPage.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppDETAug2022.Services;
+
+
+namespace WebAppDETAug2022.Pages
+{
+    public class  HeloPageModel: PageModel
+    {
+        public Helo2 ob { get; set; }
+
+        public string Message { get; set; }
+
+        public void Onget()
+        {
+            ob = new Helo2();
+            Message = ob.SayHelo("Abhilash");
+
+        }
+        
+        
+    }
+}
